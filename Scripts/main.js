f667fc9e51d3273eb3db1403ff2eca766d252493
Kemi Chris <ekeminichristopher123@gmail.com>
2023-03-19 16:41:41 +0100

    updated responsiveness

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,18 +1,28 @@
 const mobileNav = document.getElementById("mobile-nav");
 const navIcon = document.getElementById("nav-icon");
 const navIconX = document.getElementById("x-icon");
+const mobileA = document.querySelectorAll(".mobile-nav-list");
 
 
 
-navIcon.addEventListener("click", ()=>{
+navIcon.addEventListener("click", ShowMobileNav);
+
+navIconX.addEventListener("click", removeMobileNav);
+
+mobileA.forEach((mobileATag) => {
+    mobileATag.addEventListener("click", removeMobileNav)
+});
+
+function ShowMobileNav() {
     mobileNav.classList.remove("active");
     navIcon.classList.add("active");
     navIconX.classList.remove("active");
-});
+}
 
-navIconX.addEventListener("click", ()=>{
+function removeMobileNav() {
     mobileNav.classList.add("active");
     navIconX.classList.add("active");
     navIcon.classList.remove("active");
-    
-});
+}
+
+
